6f2ecee6e92e8ac7e00b7bfc8706fee8162140b2
Reza Mohammadzadeh <vscodereza@gmail.com>
2023-02-22 23:34:59 +0330

    add local custom font use @next/font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,11 @@
-import './globals.css'
-
+import "./globals.css";
+import localFont from "@next/font/local";
+const iranSansFont = localFont({
+  src: "../public/fonts/iranSans/woff2/IRANSansWeb.woff2",
+});
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fa-IR" dir="rtl" className={iranSansFont.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -10,5 +13,5 @@ export default function RootLayout({ children }) {
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
